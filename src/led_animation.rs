96607b01e6545aa020e1387cff132c2e5f7cad9a
Stattek <stattek1@pm.me>
2025-09-28 03:14:28 -0500

    WIP: making the fading elevator animation.

--- src/led_animation.rs
+++ src/led_animation.rs
@@ -1,5 +1,6 @@
 pub mod basic_pixel_sequence_animation;
 pub mod basic_pixel_sequences;
+pub mod fading_snake_animation;
 pub mod rainbow_animation;
 pub mod rainbow_snake_animation;
 pub mod single_led_fade_animation;
